1fdd3786943387fff429b43b8e8b25323ec66439
Erik Golinelli <erik@codekraft.it>
2024-05-29 19:47:35 +0200

    enables theme colors setup

--- src/edit.js
+++ src/edit.js
@@ -1,6 +1,60 @@
-import { useBlockProps } from '@wordpress/block-editor';
-import { props } from './constants';
+import {InspectorControls, useBlockProps} from '@wordpress/block-editor';
+import {props} from './constants';
+import {shadow} from "@wordpress/icons";
+import {ColorPalette, PanelBody, PanelRow, SelectControl} from '@wordpress/components';
+import {__} from '@wordpress/i18n';
+import {useSelect} from '@wordpress/data';
 
-export function edit() {
-	return ( <div {...useBlockProps( props )} /> );
+function ColorControl({value, setColor}) {
+	return (
+		<ColorPalette
+			colors={colors}
+			value={value}
+			onChange={(color) => setColor(color)}
+		/>
+	);
+}
+
+export function edit({attributes, setAttributes}) {
+	// get the template colors using wp data
+	const templateColors = useSelect('core/block-editor', []).getSettings()?.colors ?? [];
+
+	return (
+		<div {...useBlockProps()}>
+			<InspectorControls key="dark-mode">
+				<PanelBody
+					title={__('Options', 'dark-mode-block')}
+					icon={shadow}
+					initialOpen={true}
+				>
+					<PanelRow>
+						<SelectControl
+							label={__('Dark Color', 'dark-mode-block')}
+							value={attributes.darkColor}
+							options={templateColors.map(color => ({label: color.name, value: color.slug}))}
+							onChange={(value) => setAttributes({darkColor: value})}
+						/>
+					</PanelRow>
+					<SelectControl
+						label={__('Background Variable to replace', 'dark-mode-block')}
+						value={attributes.backgroundVar}
+						options={templateColors.map(color => ({label: color.name, value: color.color}))}
+						onChange={(value) => setAttributes({backgroundVar: value})}
+					/>
+					<PanelRow>
+						<SelectControl
+							label={__('Light Color', 'dark-mode-block')}
+							value={attributes.lightColor}
+							options={templateColors.map(color => ({label: color.name, value: color.slug}))}
+							onChange={(value) => setAttributes({lightColor: value})}
+						/>
+					</PanelRow>
+					{/**
+					 * @todo: add text control for text color
+					 */}
+				</PanelBody>
+			</InspectorControls>
+			<div className={props.className}></div>
+		</div>)
+		;
 }
--- src/index.js
+++ src/index.js
@@ -4,13 +4,12 @@ import { shadow } from '@wordpress/icons';
 import './style.scss';
 
 /* Block settings */
-import blockConfig from '../block.json';
+import blockConfig from './block.json';
 import { edit } from './edit';
 import { save } from './save';
 
-// Register the custom block
+/* Register the custom block */
 registerBlockType( blockConfig.name, {
-	...blockConfig,
 	icon: shadow,
 	edit,
 	save,
