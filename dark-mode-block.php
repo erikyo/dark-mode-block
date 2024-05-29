018b9ea807ee46be9ef0b9c4a5cca5cc87f65a06
Erik Golinelli <erik@codekraft.it>
2024-05-29 19:41:43 +0200

    moves block json into src folder

--- dark-mode-block.php
+++ dark-mode-block.php
@@ -1,11 +1,10 @@
 <?php
-
 /**
  * Plugin Name:       Dark Mode Block
  * Description:       Enable dark mode for your website without any effort
  * Version:           0.1.1
  * Requires at least: 5.7
- * Tested up to:      6.4
+ * Tested up to:      6.5
  * Requires PHP:      5.6
  * Author:            codekraft
  * Author URI:        https://codekraft.it
@@ -13,14 +12,13 @@
  * License URI:       https://www.gnu.org/licenses/gpl-3.0.html
  * Text Domain:       dark-mode-block
  */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
 /** Register the block */
 \add_action( 'init', function () {
-	\register_block_type( __DIR__ );
+	\register_block_type( __DIR__ . '/build' );
 } );
 
 /** Enqueue the block assets */
